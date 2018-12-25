be605f6a22dc7cf252e9566ac0632e8eaf618ce4
Tawsif Aqib <tawsif.aqib@gmail.com>
2018-12-25 14:25:43 +0600

    Add JSON exception response

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,10 +3,12 @@
 namespace App\Exceptions;
 
 use Exception;
-use Illuminate\Validation\ValidationException;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 use Laravel\Lumen\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class Handler extends ExceptionHandler
@@ -31,7 +33,7 @@ class Handler extends ExceptionHandler
      * @param  \Exception  $exception
      * @return void
      */
-    public function report(Exception $exception)
+    public function report(Exception $exception): void
     {
         parent::report($exception);
     }
@@ -41,10 +43,59 @@ class Handler extends ExceptionHandler
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \Exception  $exception
-     * @return \Illuminate\Http\Response
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function render($request, Exception $exception)
+    public function render($request, Exception $exception): Response
     {
+        if ($this->isJsonRequest($request)) {
+            $response = [
+                'message' => (string) $exception->getMessage(),
+                'status'  => Response::HTTP_INTERNAL_SERVER_ERROR,
+            ];
+
+            switch (true) {
+                case $exception instanceof HttpException:
+                    $response['message'] = Response::$statusTexts[$exception->getStatusCode()];
+                    $response['status']  = $exception->getStatusCode();
+                    break;
+
+                case $exception instanceof ModelNotFoundException:
+                    $response['message'] = Response::$statusTexts[Response::HTTP_NOT_FOUND];
+                    $response['status']  = Response::HTTP_NOT_FOUND;
+                    break;
+            }
+
+            if ($this->isDebugMode()) {
+                $response['debug'] = [
+                    'exception' => get_class($exception),
+                    'trace'     => $exception->getTrace(),
+                ];
+            }
+
+            return response()->json(['error' => $response], $response['status']);
+        }
+
         return parent::render($request, $exception);
+    }
+
+    /**
+     * Determine if the request is a JSON request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return bool
+     */
+    private function isJsonRequest(Request $request): bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine if the debug mode is enabled.
+     *
+     * @return bool
+     */
+    private function isDebugMode(): bool
+    {
+        return (bool) env('APP_DEBUG');
     }
 }
