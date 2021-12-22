901870422ac01755fe8304a02d5d3582cd5e9cbb
Tawsif Aqib <tawsif.aqib@gmail.com>
2021-12-22 21:06:23 +0100

    Fix test

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -15,6 +15,6 @@ class ExampleTest extends TestCase
     public function testExample()
     {
         $this->get('/');
-        $this->assertResponseStatus(400);
+        $this->assertResponseOk();
     }
 }
